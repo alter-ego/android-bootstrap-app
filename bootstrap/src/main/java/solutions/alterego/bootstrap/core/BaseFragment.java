3a27acfe9b33ff5ad7e76916281b05a91ab5ef7c
Sasa Sekulic <ja@sasasekulic.info>
2015-07-04 23:29:07 +0200

    added Butterknife.reset to BaseFragment.onDestroyView()

--- bootstrap/src/main/java/solutions/alterego/bootstrap/core/BaseFragment.java
+++ bootstrap/src/main/java/solutions/alterego/bootstrap/core/BaseFragment.java
@@ -5,11 +5,19 @@ import com.hannesdorfmann.fragmentargs.FragmentArgs;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
+import butterknife.ButterKnife;
+
 public class BaseFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FragmentArgs.inject(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
