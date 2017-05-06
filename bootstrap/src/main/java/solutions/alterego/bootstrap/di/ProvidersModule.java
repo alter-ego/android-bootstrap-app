289bb0d8ff9290b301eabc28d8adcdd243bd0755
Sasa Sekulic <ja@sasasekulic.info>
2017-05-06 23:59:59 +0200

    added DateTimeSerializer to Gson setup

--- bootstrap/src/main/java/solutions/alterego/bootstrap/di/ProvidersModule.java
+++ bootstrap/src/main/java/solutions/alterego/bootstrap/di/ProvidersModule.java
@@ -7,6 +7,9 @@ import com.alterego.advancedandroidlogger.implementations.AndroidLogger;
 import com.alterego.advancedandroidlogger.implementations.DetailedAndroidLogger;
 import com.alterego.advancedandroidlogger.interfaces.IAndroidLogger;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+
 import javax.inject.Named;
 import javax.inject.Singleton;
 
@@ -14,16 +17,20 @@ import dagger.Module;
 import dagger.Provides;
 import okhttp3.logging.HttpLoggingInterceptor;
 import solutions.alterego.bootstrap.BuildConfig;
+import solutions.alterego.bootstrap.util.DateTimeSerializer;
 
 @Module
 public class ProvidersModule {
 
     public static final String LOGGING_TAG = "BOOTSTRAP";
 
+    public static DateTimeSerializer dateTimeSerializer = new DateTimeSerializer(ISODateTimeFormat.dateTimeParser().withZoneUTC());
+
     @Provides
     @Singleton
     Gson provideGson() {
         return new GsonBuilder()
+                .registerTypeAdapter(DateTime.class, dateTimeSerializer)
                 .excludeFieldsWithoutExposeAnnotation()
                 .create();
     }
